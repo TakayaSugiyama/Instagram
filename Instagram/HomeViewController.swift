b83130253d58f4d65557911fae52cccadef7ccec
takayasugiyama <s.takaya1027@gmail.com>
2020-09-17 04:16:41 +0900

    9-2まで

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    // 投稿データを格納する配列
+       var postArray: [PostData] = []
 
+       override func viewDidLoad() {
+           super.viewDidLoad()
+
+           tableView.delegate = self
+           tableView.dataSource = self
+
+           // カスタムセルを登録する
+           let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+           tableView.register(nib, forCellReuseIdentifier: "Cell")
+       }
+
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return postArray.count
+       }
+
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           // セルを取得してデータを設定する
+           let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
+           cell.setPostData(postArray[indexPath.row])
+
+           return cell
+       }
 
 }
 
